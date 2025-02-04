f36cbfc564f0ee96dd426336d48c028ac5509aa8
Bro Nyaann <marketingsh004@gmail.com>
2025-02-03 17:43:11 -0800

    Add files via upload

---
+++ menu_option_2.py
@@ -0,0 +1,185 @@
+"""
+
+## Jan 2025
+
+1. Membuat Marketplace
+    progress:
+        - sudah dibuat namun masih sangat kecil
+        - perlu foto2, video & deskripsi produk (tidak copy-paste)
+    
+    target: omset 10jt | baru tercapai 3jt-an
+
+2. mengambil foto & video produk untuk di marketplace
+    progress: baru terkumpul 27 unique produk yg sudah dibuat foto-video
+    target: 
+        
+3. Memperbarui katalog whatsapp | orderan masuk via Whatsapp, lalu diarahkan agar order ke marketplace
+    progress: baru 7 produk, sedang proses membuat gambar baru diupload semua nanti
+
+
+
+. diskusi / mengobrol dengan beberapa orang & membaca2 artikel seputar tips di marketplace
+    progress: 
+        - SEO (search engine optimization), memilih kata kunci yang   & LSI (latent semantic indexing), menambahkan keyword yg berkaitan dengan produk, misal 'EtawaFresh' menjadi 'Sehato EtawaFresh 500gr - Susu Etawa Kesehatan Tulang'
+            - hasilnya: belum terlihat 
+        - Berdasarkan manual/panduan Shopee & info dari Ridwan, penggunaan hashtag secara teori berpengaruh
+            - hasilnya:  belum terlihat (Ridwan sendiri menginfokan jarang menggunakan hashtag karena hasilnya tidak terlihat)
+        - info dari ridwan, peranan paling utama/paling kuat di algoritma pencarian//penjualan adalah dari iklan, rating & penjualan
+        
+
+2. 
+
+2. 
+
+
+
+
+## kendala (secara umum):
+    1. kepercayaan publik masih kurang - rating toko masih sangat sedikit
+        - langkah yg memungkinkan membantu dengan cepat:
+            - Orderan/Penjualan produk untuk karyawan dialihkan via marketplace
+            - ada 1 akun shopee untuk melakukan checkout
+            - fisik produk bisa langsung berikan ke karyawan (pembeli)
+            - paket yg dikirim ke kurir hanya bubble wrap + resi pengiriman
+            - alamat penerima ke gudang selang (agar bubble wrap bisa dipakai kembali & hanya perlu dipasang resi baru jika ada karyawan yg belanja lagi)
+            - harga jual produk cost + 11% (administrasi marketplace non star seller)
+
+    2. masih terkendala untuk proses pengambilan foto, video & pembuatan flyer/gambar yg menarik
+        - kemampuan kami dalam mengedit & design yg masih minim
+        - produk agak lama untuk disiapkan saat ingin diambil foto
+
+    3. skill/pengetahuan/pengalaman yg masih sedikit seputar marketplace
+
+
+# A. Marketplace produk campuran
+    Langkah yg bisa/sedang diambil:
+    1. meningkatkan kepercayaan publik
+        - membuat foto/video yg menarik (dan tidak/belum dipajang di toko lain) | pengerjaan utama saat ini | target: 100 produk dengan foto / video yg masing2
+        - rating/ulasan positif di marketplace
+            - (bisa dibantu dengan poin 'kendala' no.1 diatas)
+    
+    2. mencari tau produk yg ramai dipasaran
+        - mengunjugi toko2 di marketplace yang menjual produk2 herbal (yg stoknya tersedia di gudang) & menampilkan produk2 terlalis di toko tsb
+        - menggunakan keyword/kata kunci yg umum (misal: Cuka Apel) untuk mencari produk di marketplace
+        - melalui iklan yg sering muncul
+
+    3. aktif di media sosial
+        - membuat & rajin memposting di media sosial (sehari minimal 1x)
+        - aktif berkomentar di akun2 yg memiliki banyak follower yg berpotensi tertarik untuk membeli
+        
+    4. memasang harga dibawah marketplace2 lain (namun tetap diatas harga modal + admin/pajak marketplace)
+
+
+# B. Sehato | Memasarkan produk2 premium
+    1. Sehato Menjadi Brand Kepercayaan
+        A1. Sehingga saat mencari 'Madu', 'Susu', 'Teh', 'Obat/Suplemen', masyarakat umum akan terfikir/terbetik 'Sehato'
+            - Langkah yg bisa dilakukan:
+                - Membuat tulisan/artikel/konten berkualitas/profesional (yg tidak terlalu menunjukkan sedang berjualan) | misal spt: ust zaidul akbar, sekalipun tidak berjualan, namun saat menyebutkan obat tertentu (misal cuka apel), orang2 akan mencari/mau membeli
+                - Lebih aktif di media sosial
+                    - aktif group/komunitas yg berkaitan dengan 'Obat/Suplemen Herbal', 'Kesehatan', dsb
+                    - aktif di akun orang2 yg berpengaruh/memiliki banyak follower yg berpotensi tertarik dengan produk2 Sehato
+        
+        A2. Verified Account (checklist biru)
+        
+        A3. Mendapatkan follower lebih banyak / brand awareness yg lebih tinggi
+            - give away
+                - dengan syarat memfollow & share postingan Sehato Official
+            - meningkatkan interaksi
+                - tanya-jawab/quiz/pooling
+                - 
+
+    2. Brand Sehato dapat dipasarkan di Amazon, Ebay / Marketplace berskala Internasional
+
+    - Langkah yg belum bisa dilakukan - Sehato Official:
+        - affiliate
+        - ads
+        - menyebar flyer
+
+
+- langkah yg belum bisa diambil
+    1. menekan cost / mencari supplier produk2 tsb yg bisa memberi harga termurah
+    2. 
+
+## Target
+    - menjadi Star Seller dalam waktu 3 bulan (akhir April 2025) | 
+
+
+
+
+
+## Kegiatan - Jan 2025
+
+- Belajar membuat foto & video produk untuk di marketplace
+    - tujuan: membuat kesan yang lebih menarik, agar customer lebih tertarik (setidaknya untuk meng-klik)
+
+- Membaca-baca seputar SEO (search engine optimization), LSI (Latent Semantic Indexing) untuk di shopee
+    - tujuan: agar produk di shopee lebih berpotensi tampil di pencarian
+
+- Mencari -secara online- komunitas/group herbal, kesehatan, obat2-an dari penyakit tertentu yg berpeluang untuk memperkenalkan produk2 sentra herbal
+    - membuat postingan/share artikel seputar kesehatan | tujuan: meningkatkan kepercayaan/membuat publik 
+
+- Memposting-posting produk di media sosial
+    - tujuan: memperkenalkan produk2 via sosial media
+
+- Memperbarui Catalog Produk di Whatsapp
+    - 
+- Broadcast pesan berisi flyer ke karyawan
+
+- Menjawab pesan2/pertanyaan yg masuk (kebanyakan menanyakan produk yg tidak dijual di Sentra Herbal)
+
+- Broadcast/memberikan penawaran produk ke calon customer
+
+
+## Yg perlu ditingkatkan
+
+1. Rating/Review Toko
+
+    Langkah:
+    - Penjualan produk ke karyawan dialihkan via marketplace
+        - ada 1 akun shopee untuk melakukan checkout
+        - fisik produk bisa langsung berikan ke karyawan (pembeli)
+        - resi/paket yg dikirim ke kurir hanya bubble wrap
+        - alamat penerima ke gudang selang (agar bubble wrap bisa dipakai kembali & hanya perlu dipasang resi baru jika ada karyawan yg belanja lagi)
+        - harga jual produk cost + 12% (administrasi marketplace)
+
+2. Foto & Video Produk
+    Langkah:
+    1. Dapat mengambil foto yg berkualitas sehingga nama/brand lebih terlihat menarik bagi calon customers | currently: low
+    2. Dapat membuat gambar & video yg menarik  | currently: low
+        - Video:
+            - singkat (durasi tidak lebih dari 30 detik)
+            - menyampaikan isi produk
+                - manfaat
+                - komposisi
+                - izin edar
+                - cara/aturan pakai
+
+"""
+
+"""
+
+"""
+
+
+"""
+## 
+Target MP brand/produk umum
+
+Target MP produk premium
+Target utama
+
+
+## Yang perlu dipenuhi/dicapai dibulan berikutnya
+1. Omset lebih tinggi dari bulan sebelumnya
+2. Dapat mengambil foto yg berkualitas sehingga nama/brand lebih menarik bagi para calon customer | now: low
+3. Dapat membuat gambar & video yg menarik  | now: low
+    - Video:
+        - singkat (durasi tidak lebih dari 30 detik)
+        - menyampaikan isi produk
+            - manfaat
+            - komposisi
+            - izin edar
+            - cara/aturan pakai
+
+4. 
+"""
