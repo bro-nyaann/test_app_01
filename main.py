7c6c8e2dce51579af80ddce32ec51720cdd0175e
Bro Nyaann <marketingsh004@gmail.com>
2025-02-03 17:52:42 -0800

    Update main.py

--- main.py
+++ main.py
@@ -19,54 +19,57 @@ def main():
 	## hide streamlit header & footer styles
 	utils.fn_hide_streamlit_styles()
 
-	## load the raw data into pandas dataframe
-	df_raw = utils.csv_to_df("raw_data/sales_data.csv")
-	
-	df_raw_2 = utils.fn_split_datetime_column(df_raw, "Order Lines/Created on")
-	df_raw_2 = utils.fn_rename_columns(df_raw_2)
-	df_raw_2 = utils.fn_add_week_index(df_raw_2)
+	password = st.text_input("Enter your password:", type="password")
+	if password == 'SentraHerbal':
 
-	## horizontal nav menu ##
-	menu_options = (
-		"Home",
-		"Penjualan",
-		"Target",
-		"Kendala",
-		"Ringkasan",
-		'---',
-	)
+		## load the raw data into pandas dataframe
+		df_raw = utils.csv_to_df("raw_data/sales_data.csv")
+		
+		df_raw_2 = utils.fn_split_datetime_column(df_raw, "Order Lines/Created on")
+		df_raw_2 = utils.fn_rename_columns(df_raw_2)
+		df_raw_2 = utils.fn_add_week_index(df_raw_2)
 
-	with st.sidebar:
-		picked_option = option_menu(
-			"Index",
-			menu_options,
-			icons=None,
-			menu_icon="cast",
-			default_index=0,
-			orientation="vertical")
-		st.write('---')
+		## horizontal nav menu ##
+		menu_options = (
+			"Home",
+			"Penjualan",
+			"Target",
+			"Kendala",
+			"Ringkasan",
+			'---',
+		)
 
-	# col1, col2 = st.columns()
-	# with col1:
-	# 	st.write('hello world')
+		with st.sidebar:
+			picked_option = option_menu(
+				"Index",
+				menu_options,
+				icons=None,
+				menu_icon="cast",
+				default_index=0,
+				orientation="vertical")
+			st.write('---')
 
-	# with col2:
-	# 	st.dataframe(df_raw_2)
+		# col1, col2 = st.columns()
+		# with col1:
+		# 	st.write('hello world')
 
-	if picked_option == menu_options[0]:
-		fn_menu_option_0(df_raw_2)
+		# with col2:
+		# 	st.dataframe(df_raw_2)
 
-	elif picked_option == menu_options[1]:
-		fn_menu_option_1(df_raw_2)
+		if picked_option == menu_options[0]:
+			fn_menu_option_0(df_raw_2)
 
-	elif picked_option == menu_options[2]:
-		pass
+		elif picked_option == menu_options[1]:
+			fn_menu_option_1(df_raw_2)
 
-	elif picked_option == menu_options[3]:
-		pass
+		elif picked_option == menu_options[2]:
+			pass
 
-	elif picked_option == menu_options[4]:
-		pass
+		elif picked_option == menu_options[3]:
+			pass
+
+		elif picked_option == menu_options[4]:
+			pass
 		
 if __name__ == '__main__':
 	main()
